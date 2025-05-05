44bb06a1f9c823ec4ee4e5f90ddb57c20f1f17e8
muslum <muslumdogan01@gmail.com>
2025-05-05 12:19:24 +0100

    bug fix

--- wareact-frontend/src/app/page.tsx
+++ wareact-frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/hero/page";
 
 export default function Home() {
   return (
-   <div className="w-full flex flex-col ">
+   <div className="w-full flex flex-col relative z-40">
      <Hero/>
    </div>
   );
